e7819fad540af7f33ebf15fa8ad80227644b3346
imran-hossain <engg.imran22@gmail.com>
2024-01-03 18:45:38 +0600

    database connect

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
-
+const path = require("path")
+const cookieParser = require("cookie-parser")
 const app = express()
 
 dotenv.config()
-app.get('/', (req,res)=>{
-    res.send('Server create and test .')
+ 
+// Data base connection 
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{
+    useNewUrlParser : true,
+    useUnifiedTopology:true,
 })
-app.listen(3000,()=>{
-    console.log("Server is running");
+
+.then(()=>console.log("database connection successfullay"))
+.catch((err)=>console.log(err))
+
+// requset parser 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// set view engine
+
+app.set("view engine",'"ejs')
+
+// set static folder 
+
+app.use(express.static(path.join(__dirname,"public")))
+
+// cookieparser 
+
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
+// routing setup
+
+
+// error handaling
+
+app.listen(process.env.PORT,()=>{
+    console.log(`app listening to port ${process.env.PORT}`);
 })
+
+
